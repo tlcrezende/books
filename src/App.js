5181b0edd4cf131c31e9fd7f55e3ce9102e3f99b
Thiago <tlcrezende@gmail.com>
2023-02-05 18:51:38 -0300

    add delete book

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+        return book.id !== id;
+      });
+
+      setBooks(updatedBooks);
+    }
 
   const createBook = (title) => {
     const updatedBooks = [
@@ -14,7 +23,8 @@ function App() {
 
   };
 
-  return <div>
+  return <div className="app">
+    <BookList books={books} onDelete={deleteBookById}/>
     <BookCreate onCreate={createBook} />
   </div>;
 }
