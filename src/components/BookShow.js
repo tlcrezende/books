f7962e44676f66893dcef505ad108a862dc37300
Thiago <tlcrezende@gmail.com>
2023-02-05 20:52:47 -0300

    Add edit painel

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit'
+
 function BookShow({book, onDelete}) {
-  const handleClick = () => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const deleteClick = () => {
     onDelete(book.id);
   }
 
+  const editClick = () => {
+    setShowEdit(!showEdit);
+  }
+
+  let content = <h3>{book.title}</h3>
+  if (showEdit) {
+    content = <BookEdit book={book}/>
+  }
 
   return <div className="book-show">
-      {book.title}
+      <div>{content}</div>
       <div className="actions">
-        <button className="delete" onClick={handleClick}>
+        <button className="edit" onClick={editClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={deleteClick}>
           Delete
         </button>
       </div>
